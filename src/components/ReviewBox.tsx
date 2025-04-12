dd2544984082139e216cd25d7f0bb49681ef370a
Nuttamas Udomsantitham <6733064521@student.chula.ac.th>
2025-04-12 14:23:30 +0700

    Add Review Box

---
+++ src/components/ReviewBox.tsx
@@ -0,0 +1,32 @@
+import { TextField } from "@mui/material";
+import React from "react";
+import Box from '@mui/material/Box';
+
+export default function ReviewBox() {
+    return (
+        <Box 
+            component = "form"
+            sx= {{ '& .MuiTextField-root': { 
+                m: 2, 
+                width: '50ch'
+                } 
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <div>
+                <div className="text-xl font-bold">
+                    Review
+                </div>
+                <TextField
+                    id="outlined-multiline-static"
+                    // label="Multiline"
+                    multiline
+                    rows={4}
+                    placeholder={`How was your dining experience?\nTell us what you loved (or didn’t)!`}
+                />
+            </div>
+
+        </Box>
+    );
+}
