afb245fda3ef0c811444e70db74ce3cb66fee545
Nuttamas Udomsantitham <6733064521@student.chula.ac.th>
2025-04-13 13:42:51 +0700

    Verify Page

---
+++ src/app/(admininfo)/admin/verify/page.tsx
@@ -0,0 +1,14 @@
+import VerifyCard from "@/components/VerifyCard";
+
+export default function Verify() {
+    return (
+        <main>
+            <div className="flex flex-col pl-7 space--5 bg-myred pb-7 text-3xl font-bold text-white pt-7">
+                Verify Restaurant Accounts
+            </div>
+            <div>
+                <VerifyCard/>
+            </div>
+        </main>
+    );
+}
