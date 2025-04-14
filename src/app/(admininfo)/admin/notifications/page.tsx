eba348274d52857bb27402de84c859fc4b832083
Supanyada Puttharak <6733260121@student.chula.ac.th>
2025-04-14 16:55:36 +0700

    Edit notifications page for admin

--- src/app/(admininfo)/admin/notifications/page.tsx
+++ src/app/(admininfo)/admin/notifications/page.tsx
@@ -2,15 +2,8 @@ import AdminNotificationRow from "@/components/AdminNotificationRow";
 
 export default function Notification() {
     return (
-        // <main className="bg-[#D40303] h-[100%] pb-10">
-            
-        //     <div className='flex flex-col items-center space--5'>
-        //     <div className="text-3xl font-bold text-white pt-10">
-        //         Notification
-                
-        //     </div>
-        //     </div>
-        // </main>
-        <AdminNotificationRow/>
+        <main>
+            <AdminNotificationRow/>
+        </main>
     );
 }
--- src/components/AdminNotificationRow.tsx
+++ src/components/AdminNotificationRow.tsx
@@ -1,4 +1,5 @@
 import AdminNotificationBox from "./AdminNotificationBox";
+import AdminNotificationPanel from "./AdminNotificationPanel";
 import getNotifications from "@/libs/getNotifications";
 import { NotificationJson, NotificationItem } from "../../interfaces";
 import getUserProfile from "@/libs/getUserProfile";
@@ -16,9 +17,7 @@ export default async function AdminNotificationRow() {
 
   return (
     <main className="w-screen m-0 p-0">
-      <div className="w-screen flex justify-between items-center h-[80px] px-6 bg-red-600 border-b border-red-700">
-        <h1 className="text-2xl font-bold text-white">Notifications</h1>
-      </div>
+      <AdminNotificationPanel/>
       {notificationJson.data.map((notificationItem: NotificationItem) => (
         <AdminNotificationBox
           key={notificationItem._id}
