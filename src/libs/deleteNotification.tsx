5845b9e6b3d810af11a2a438ac759bb438f36384
khim7mimi <6733171021@student.chula.ac.th>
2025-04-15 10:39:13 +0700

    Co-authored-by: PPwinE55 <PPwinE55@users.noreply.github.com>

--- src/libs/deleteNotification.tsx
+++ src/libs/deleteNotification.tsx
@@ -1,5 +1,4 @@
 export default async function deleteNotification ({notificationId, token} : {notificationId : string, token : string}) {
-    console.log("notificationId :"+notificationId )
 
     const response = await fetch(process.env.BACKEND_URL+`api/v1/notifications/${notificationId}`, {
         method : 'DELETE',
@@ -7,7 +6,7 @@ export default async function deleteNotification ({notificationId, token} : {not
             'Authorization': `Bearer ${token}`,
         },
     });
-console.log(response )
+    
     if(!response.ok){
         throw new Error('Failed to delete notification');
     }
