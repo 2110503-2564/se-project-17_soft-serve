c30ed959940af8bd00039870e10dd6952e74678a
Supanyada Puttharak <6733260121@student.chula.ac.th>
2025-04-21 22:05:55 +0700

    Edit loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,9 +4,9 @@ export default function Loader({loadingtext} : {loadingtext? : string}) {
     if(!loadingtext) loadingtext = 'Loading...';
 
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen text-brown-700">
-        <img src="/icecream_melting.gif" alt="Loading..." className="w-[100px] h-[100px] mb-4" />
-        <p className="text-lg font-light animate-pulse">{loadingtext}</p>
+      <div className="flex flex-col items-center justify-center min-h-[calc(100vh-60px)]">
+        <img src="/softserve_bus.gif" alt="Loading..." className="w-[250px] h-[250px] relative top-[-15px]"/>
+        <p className="absolute text-lg text-brown-800 font-light animate-pulse z-10 mt-40">{loadingtext}</p>
       </div>
     );
   }
